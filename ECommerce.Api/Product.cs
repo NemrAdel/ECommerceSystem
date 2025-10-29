1c0c0c30f86e7c38a146cd3e3c4f8fb244d8f209
Tiger <nemradel220@yahoo.com>
2025-10-29 21:46:47 +0300

    Add project files.

---
+++ ECommerce.Api/Product.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Api
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
