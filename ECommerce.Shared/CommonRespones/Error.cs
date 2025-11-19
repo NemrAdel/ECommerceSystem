59d4e5e09b5d7720b1256e253345772c5183a84c
Tiger <nemradel220@yahoo.com>
2025-11-19 21:35:53 +0200

    save

---
+++ ECommerce.Shared/CommonRespones/Error.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Shared.CommonRespones
+{
+    public class Error
+    {
+        private Error(string code, string description, ErrorType errorType) //can't create object now from ctor
+        {
+            Code = code;
+            Description = description;
+            ErrorType = errorType;
+        }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public ErrorType ErrorType { get; set; }
+
+        public static Error Failure(string code = "General Failure", string description = "A General Failure Has Occurred")
+        {
+            return new Error(code, description, ErrorType.Failure);
+        }
+        public static Error Validation(string code = "General Failure", string description = "A General Failure Has Occurred")
+        {
+            return new Error(code , description , ErrorType.Validation);
+        }
+
+    }
+}
