e2a5ae1e3dc40d9c8a0b7dc517564ffe51d48e46
Tiger <nemradel220@yahoo.com>
2025-11-20 21:57:23 +0200

    save

--- ECommerce.Shared/CommonRespones/Error.cs
+++ ECommerce.Shared/CommonRespones/Error.cs
@@ -11,24 +11,48 @@ namespace ECommerce.Shared.CommonRespones
         private Error(string code, string description, ErrorType errorType) //can't create object now from ctor
         {
             Code = code;
-            Description = description;
+            Description = description;  
             ErrorType = errorType;
         }
         public string Code { get; set; }
         public string Description { get; set; }
         public ErrorType ErrorType { get; set; }
 
-        public static Error Failure(string code = "General Failure", string description = "A General Failure Has Occurred")
+        public static Error Failure(string code = "General.Failure", string description = "A General Failure Has Occurred")
         {
             return new Error(code, description, ErrorType.Failure);
         }
-        public static Error Validation(string code = "General Failure", string description = "A General Failure Has Occurred")
+        public static Error Validation(string code = "General.Validation", string description = "A General Validation Has Occurred")
         {
             return new Error(code , description , ErrorType.Validation);
         }
-        public static Error NotFound(string code = "General Failure", string description = "A General Failure Has Occurred")
+        public static Error NotFound(string code = "NotFound.Failure", string description = "A General NotFound Has Occurred")
         {
             return new Error(code , description , ErrorType.Validation);
+        }
+
+        public static Error UnAuthorized(
+            string code = "Genaral.UnAuthorized",
+            string description = "You are Not Authorized to perform this action"
+        )
+        {
+            return new Error(code, description, ErrorType.UnAuthorized);
+        }
+
+        public static Error Forbidden(
+            string code = "Genaral.Forbidden",
+            string description = "You don't have the access to this resource,Access denied"
+        )
+        {
+            return new Error(code, description, ErrorType.Forbidden);
+        }
+
+        public static Error InvalidCredintals(
+            string code = "Genaral.InvalidCredintals",
+            string description = "Your Credintals is not valid to reach this resource"
+        )
+        {
+            return new Error(code, description, ErrorType.InvalidCredintals);
         }
 
     }
