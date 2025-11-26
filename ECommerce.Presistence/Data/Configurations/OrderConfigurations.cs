bc9f594b472b3c4396a0ace9826d1cc43ada5b79
Tiger <nemradel220@yahoo.com>
2025-11-26 21:45:56 +0200

    save

---
+++ ECommerce.Presistence/Data/Configurations/OrderConfigurations.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.Doamin.Entities.OrderModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presistence.Data.Configurations
+{
+    public class OrderConfigurations : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.Property(x => x.SubTotal).HasColumnType("decimal(8,2)");
+            builder.OwnsOne(x => x.Address, OE => {
+                OE.Property(x => x.FirsName).HasMaxLength(100);
+                OE.Property(x => x.LastName).HasMaxLength(100);
+                OE.Property(x => x.City).HasMaxLength(100);
+                OE.Property(x => x.Street).HasMaxLength(100);
+                OE.Property(x => x.Country).HasMaxLength(100);
+            };
+        }
+    }
+}
---
+++ ECommerce.Presistence/Data/Configurations/OrderItemConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Doamin.Entities.OrderModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presistence.Data.Configurations
+{
+    public class OrderItemConfigurations : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.Property(x => x.Price).HasColumnType("decimal(8,2)");
+            builder.OwnsOne(x => x.Product, oe =>
+            {
+                oe.Property(x => x.ProductName).HasMaxLength(50);
+            }
+            );
+        }
+    }
+}
