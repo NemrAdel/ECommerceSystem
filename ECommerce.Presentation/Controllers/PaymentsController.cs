5bb18b964d14dc61d4430aa85071d00271d674e9
Tiger <nemradel220@yahoo.com>
2025-12-08 18:24:25 +0200

    save

--- ECommerce.Presentation/Controllers/PaymentsController.cs
+++ ECommerce.Presentation/Controllers/PaymentsController.cs
@@ -1,11 +1,6 @@
 ﻿using ECommerce.Services.Abstraction;
 using ECommerce.Shared.DTOs.BasketDTOs;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ECommerce.Presentation.Controllers
 {
