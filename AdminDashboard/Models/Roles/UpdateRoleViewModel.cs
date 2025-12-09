a7cb954cd782771ef365dfdb5c82721459e71443
Nemr <nemradel220@yahoo.com>
2025-12-10 01:06:16 +0200

    save

---
+++ AdminDashboard/Models/Roles/UpdateRoleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AdminDashboard.Models.Roles
+{
+    public class UpdateRoleViewModel
+    {
+        public string Id { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public bool IsSelected { get; set; } = default!;
+    }
+}
