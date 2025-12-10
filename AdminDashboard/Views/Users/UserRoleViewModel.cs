04650b44ac9e30dcf0263f438869a5eb710d8d65
Tiger <nemradel220@yahoo.com>
2025-12-10 21:42:36 +0200

    save

---
+++ AdminDashboard/Views/Users/UserRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using AdminDashboard.Models.Roles;
+
+namespace AdminDashboard.Views.Users
+{
+    public class UserRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public List<UpdateRoleViewModel> Roles { get; set; }
+
+    }
+}
