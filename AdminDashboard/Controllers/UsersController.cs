998c63c7bd97145cc1af0ea297ba6695f6da6086
Tiger <nemradel220@yahoo.com>
2025-12-10 18:15:15 +0200

    save

---
+++ AdminDashboard/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using AdminDashboard.Models.Users;
+using ECommerce.Doamin.Entities.IdentityModule;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace AdminDashboard.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager,RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = _userManager.Users.Select(user => new UserViewModel
+            {
+                Id = user.Id,
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                UserName = user.UserName,
+                Roles = _userManager.GetRolesAsync(user).Result
+            }).ToListAsync();
+
+            return View(users);
+        }
+    }
+}
